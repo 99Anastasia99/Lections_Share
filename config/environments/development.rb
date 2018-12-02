cf9696da031efa23d7eced2c616e0d488cb70bea
Nastassia Szymanskaya <99nastyusha99@gmail.com>
2018-12-03 00:41:43 +0300

    confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,20 +7,11 @@ Rails.application.configure do
   config.cache_classes = false
   config.action_cable.mount_path = "/cable"
 
-  # General Settings
-  #   config.app_domain = 'localhost::3000'
-  #   config.action_mailer.default_url_options = { host: 'localhost:3000'}
-  #   # Email
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer_perform_deliveries = true
-  # config.action_mailer.smtp_settings = {
-  #   address:              'smtp.gmail.com',
-  #   port:                 587,
-  #   domain:               'localhost:3000',
-  #   user_name:            '<99nastyusha99>',
-  #   password:             '<Vkontakte99>',
-  #   authentication:       'plain',
-  #   enable_starttls_auto: true }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   # Do not eager load code on boot.
   config.eager_load = false
 
