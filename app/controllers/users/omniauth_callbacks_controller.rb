94d351cc1c21caa97a4913767b3c53a641e302ce
Nastassia Szymanskaya <99nastyusha99@gmail.com>
2018-12-03 15:03:07 +0300

    omniauth revise

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,4 @@
-class Users::OmniauthController < ApplicationController
+class Users::OmniauthCallbacksController < ApplicationController
   def facebook
   @user = User.from_omniauth(request.env['omniauth.auth'])
   if @user.persisted?
