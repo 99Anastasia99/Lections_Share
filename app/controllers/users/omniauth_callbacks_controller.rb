d6a4337a5f18e953bc4cfa878b3a6246c29ee91b
Nastassia Szymanskaya <99nastyusha99@gmail.com>
2018-12-03 15:44:07 +0300

    omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,40 +1,42 @@
 class Users::OmniauthCallbacksController < ApplicationController
   def facebook
-  @user = User.from_omniauth(request.env['omniauth.auth'])
-  if @user.persisted?
-    sign_in_and_redirect @user
-    set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-  else
-    flash[:error] = 'There was a problem signing you in through Facebook. Please register or try signing in later.'
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+      flash[:notice] = 'Logged via Facebook'
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def twitter
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    if @user.persisted?
+      sign_in_and_redirect @user
+      set_flash_message(:notice, :success, kind: 'Twitter') if is_navigational_format?
+    else
+      flash[:error] = 'There was a problem signing you in through Twitter. Please register or try signing in later.'
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def vkontakte
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    if @user.persisted?
+      sign_in_and_redirect @user
+      set_flash_message(:notice, :success, kind: 'Vkontakte') if is_navigational_format?
+    else
+      flash[:error] = 'There was a problem signing you in through Vkontakte. Please register or try signing in later.'
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    flash[:error] = 'There was a problem signing you in. Please register or try signing in later.'
     redirect_to new_user_registration_url
   end
-end
-
-def twitter
-  @user = User.from_omniauth(request.env['omniauth.auth'])
-  if @user.persisted?
-    sign_in_and_redirect @user
-    set_flash_message(:notice, :success, kind: 'Twitter') if is_navigational_format?
-  else
-    flash[:error] = 'There was a problem signing you in through Twitter. Please register or try signing in later.'
-    redirect_to new_user_registration_url
-  end
-end
-
-def vkontakte
-  @user = User.from_omniauth(request.env['omniauth.auth'])
-  if @user.persisted?
-    sign_in_and_redirect @user
-    set_flash_message(:notice, :success, kind: 'Vkontakte') if is_navigational_format?
-  else
-    flash[:error] = 'There was a problem signing you in through Vkontakte. Please register or try signing in later.'
-    redirect_to new_user_registration_url
-  end
-end
-
-def failure
-  flash[:error] = 'There was a problem signing you in. Please register or try signing in later.'
-  redirect_to new_user_registration_url
-end
 
 end
