793b8e593d2cab80c2d5b63744b0e6a21cb6e4df
Nastassia Szymanskaya <99nastyusha99@gmail.com>
2018-12-03 15:27:33 +0300

    validates

--- app/models/lection.rb
+++ app/models/lection.rb
@@ -4,8 +4,9 @@ class Lection < ApplicationRecord
   has_many :ratings, dependent: :destroy
   acts_as_taggable_on :tags
   belongs_to :user, optional: true
-  validates :title,presence: true, uniqueness: { scope: :user_id, message: "must be unique for each of your posts" }
-  validates :body,presence: true,length: {maximum: 1500},on: :create,allow_nil: false
+  validates :title,length: {maximum: 255}, presence: true, uniqueness: { scope: :user_id, message: "must be unique for each of your posts" }
+  validates :body,presence: true,length: {maximum: 45000},on: :create,allow_nil: false
+  validates :speciality, length: {maximum: 20}
 
   def current_user_rating current_user
     if current_user_rating = self.ratings.find_by(user_id: current_user.id)
